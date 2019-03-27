ccf29557b516606514a58c599d297f83a4101c03
aguyon <alexisguyon01@gmail.com>
2019-03-27 17:39:20 +0100

    Count + show/hide img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
+import homerMarge from './homerMarge.png'
 import './App.css';
 
 import Quotes from "./Quotes";
@@ -9,29 +10,56 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      working: true
+      working: true,
+      show: true,
+      count: 0
     };
   }
   handleClick = () => {
     this.setState({ working: !this.state.working });
   };
+  showMarge = () => {
+    this.setState({ show: !this.state.show });
+  };
+  incrementCounter = (increment) => {
+    const count = this.state.count + increment;
+    if(count >= 0) { this.setState({count}) }
+  };
   render() {
     const homer = this.state.working ? 'Working' : 'Not Working';
     const logoReact = this.state.working ? 'logoWorking' : 'logoNotWorking';
+    const marge = this.state.show ? 'Hide' : 'Right Here';
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className={logoReact} alt="logo" />
           <h1 className="App-title">Simpson Quotes</h1>
         </header>
-        <div className="container">
-          <h2>Is Homer Working ?</h2>
+
+        <h2>Compteur : { this.state.count }</h2>
+          <button className="btnCount" onClick={() => this.incrementCounter(1)}>
+            Plus
+          </button>
+          <button className="btnCount" onClick={() => this.incrementCounter(-1)}>
+            Moins
+          </button>
+
+        <h2>Is Homer Working ?</h2>
           <button className="buttonWork" onClick={this.handleClick}>
             {homer.toUpperCase()}
           </button>
-          {/* <Lamp on /> */}
-          <Quotes />
-        </div>
+
+        {/* <Lamp on /> */}
+
+        <Quotes />
+
+        <h2>And Marge, where is she ?</h2>
+        <button className="btnMarge" onClick={(this.showMarge)}>
+          {marge.toUpperCase()}
+        </button>
+        <br />
+        {this.state.show ? <img src={homerMarge} alt='' /> : null}
       </div>
     );
   }
